a7440d07a240a699bc142501e9fb0f22afae2676
Aswinnath TE <aswinnathte125@gmail.com>
2025-08-02 16:34:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
     try {
       setUploadStatus("Uploading...");
-      const response = await axios.post("https://image-upload-backend-v12y.onrender.com/upload-image", formData, {
+      const response = await axios.post("https://image-upload-backend-v12y.onrender.com/upload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
